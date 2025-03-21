9ad7d335d7aa86e662a79d8872e353fdb9f2ef8e
Abhishek Patil <patilabhishek394@gmail.com>
2025-03-21 09:16:04 +0530

    fix: remove default icons and images, insert own custom images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import weatherLogo from "./assets/icons/weather.png";
+import weatherLogoPublic from "/weather.png";
 import "./App.css";
 
 function App() {
@@ -10,10 +10,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={weatherLogoPublic} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={weatherLogo} className="logo react" alt="Weather logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
